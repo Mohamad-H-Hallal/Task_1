4a5b00b30cde03d9653b653bcb693092c829cb5e
User <User@AMATERASU-PC>
2023-06-16 16:33:38 +0300

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a Small Quiz!");
+            Console.WriteLine("What is 1+1 equal to?");
+            String v = Console.ReadLine();
+            if (v == "2")
+            {
+                Console.WriteLine("Congrats your answer is correct!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! Try again next time.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
